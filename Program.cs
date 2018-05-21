dc9ddc7d32471c8c9fd835950060d79b8fd7ca0b
Jessie Engstrom <jessielengstrom@gmail.com>
2018-05-21 10:25:18 -0700

    dynamically created alphabet dictionary instead of writing it out

--- Program.cs
+++ Program.cs
@@ -9,35 +9,9 @@ namespace Anagram_Test
         static int check_anagram(String a)
         {
             // give each character in the alphabet a weight
-            var alphabet = new Dictionary<char, int>
-            {
-                ['a'] = 1,
-                ['b'] = 2,
-                ['c'] = 3,
-                ['d'] = 4,
-                ['e'] = 5,
-                ['f'] = 6,
-                ['g'] = 7,
-                ['h'] = 8,
-                ['i'] = 9,
-                ['j'] = 10,
-                ['k'] = 11,
-                ['l'] = 12,
-                ['m'] = 13,
-                ['n'] = 14,
-                ['o'] = 15,
-                ['p'] = 16,
-                ['q'] = 17,
-                ['r'] = 18,
-                ['s'] = 19,
-                ['t'] = 20,
-                ['u'] = 21,
-                ['v'] = 22,
-                ['w'] = 23,
-                ['x'] = 24,
-                ['y'] = 25,
-                ['z'] = 26
-            };
+            Dictionary<char, int> alphabet = new Dictionary<char, int> {};
+            for (char c = 'a'; c <= 'z'; c++)
+                alphabet[c] = c - 64;
 
             // return the total weight of a word
             var counts = 0;
--- anagram.py
+++ anagram.py
@@ -1,3 +1,5 @@
+from string import ascii_lowercase as al
+
 def get_words(file):
     """Open and parse word file."""
 
@@ -15,34 +17,7 @@ def count_words(word):
     """Get the count of a word."""
 
     # give each character a weight
-    chars = {
-        'a': 1,
-        'b': 2,
-        'c': 3,
-        'd': 4,
-        'e': 5,
-        'f': 6,
-        'g': 7,
-        'h': 8,
-        'i': 9,
-        'j': 10,
-        'k': 11,
-        'l': 12,
-        'm': 13,
-        'n': 14,
-        'o': 15,
-        'p': 16,
-        'q': 17,
-        'r': 18,
-        's': 19,
-        't': 20,
-        'u': 21,
-        'v': 22,
-        'w': 23,
-        'x': 24,
-        'y': 25,
-        'z': 26
-    }
+    chars = {x:i for i, x in enumerate(al, 1)}
 
     count = 0
 
